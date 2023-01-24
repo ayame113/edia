665d22214b8f268f2eb2774c063f64c3528744fd
ayame113 <40050810+ayame113@users.noreply.github.com>
2023-01-24 22:46:18 +0900

    add build error message

--- build/check.ts
+++ build/check.ts
@@ -3,5 +3,14 @@ import { code as expected, output } from "./run.ts";
 
 const actual = await Deno.readTextFile(new URL(import.meta.resolve(output)));
 
-assertEquals(actual, expected, "Did you forget to run `deno task build`?");
+try {
+  assertEquals(actual, expected);
+} catch (error) {
+  (error as Error)
+    .message
+    .split("\n")
+    .filter((line) => line.includes("\x1b[32m") || line.includes("\x1b[31m"))
+    .forEach((line) => console.log(line));
+  throw "Did you forget to run `deno task build`?";
+}
 console.log("✅checked the build output");
